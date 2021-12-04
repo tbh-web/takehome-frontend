75c3e1bb9ca4fcd2a042d600a8051e51dae4ebdf
Brooke Krajancich <brookekrajancich@gmail.com>
2021-12-03 17:39:33 -0800

    basic setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img className="logo" src={logo} alt="tbh-logo"/>
+      <div style={{margin: "200px"}}>
+        <h1>Refer a friend!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
